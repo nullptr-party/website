c4b7c5e215099877ca001bf2267fa82dfa22f446
Ilya Gulya <ilya@gulya.me>
2024-11-11 23:26:53 +0500

    Add pages deployment. Make static export.

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,11 @@
 import type {NextConfig} from "next";
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  env: {
+    SITE_DOMAIN: process.env.SITE_DOMAIN || 'nullptr.party',
+  },
+};
 
 export default nextConfig;
