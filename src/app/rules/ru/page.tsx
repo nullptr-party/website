955c34a55e044480683d462214045e55bf339323
Ilya Gulya <ilya@gulya.me>
2024-11-12 00:12:41 +0500

    add rules

---
+++ src/app/rules/ru/page.tsx
@@ -0,0 +1,6 @@
+import RulesContent from '../_components/RulesContent';
+import { translations } from '../_data/translations';
+
+export default function RulesRuPage() {
+  return <RulesContent content={translations.ru} currentLang="ru" />;
+}
