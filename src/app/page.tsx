b2317860995ac4a375e5f9d0eb93571cb95513af
Ilya Gulya <ilya@gulya.me>
2025-07-30 13:11:07 +0500

    Remove subdomains

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,29 +1,14 @@
 // app/page.tsx
 import LandingPage from '../components/LandingPage';
 
-interface SiteConfig {
-  domain: string;
-  telegramLink?: string;
-  cityLinks?: Array<{name: string; link: string}>;
-}
-
-const siteConfigs: Record<string, SiteConfig> = {
-  'nullptr.party': {
-    domain: 'nullptr.party',
-    telegramLink: 'https://t.me/+1-aB-cGCv4pkMDAy',
-    cityLinks: [
-      {name: 'Almaty', link: 'https://t.me/+YgE_vglZYnkxMWVi'}
-    ],
-  },
-  'almaty.nullptr.party': {
-    domain: 'almaty.nullptr.party',
-    telegramLink: 'https://t.me/almaty_nullptr_party',
-  },
+const config = {
+  domain: 'nullptr.party',
+  telegramChannelLink: 'https://t.me/+1-aB-cGCv4pkMDAy',
+  cityLinks: [
+    {name: 'Almaty', link: 'https://t.me/+YgE_vglZYnkxMWVi'}
+  ],
 };
 
 export default function Home() {
-  const domain = process.env.SITE_DOMAIN as string;
-  const config = siteConfigs[domain];
-
   return <LandingPage {...config} />;
 }
